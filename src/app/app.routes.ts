02ad620dc5fd9852e44222c3e9707c271f35e12f
Tanbin Himel <tanbin.hossain@trucklagbe.com>
2024-05-13 12:55:35 +0600

    #master auth service added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import {Routes} from '@angular/router';
 import {AuthComponent} from "./auth/auth.component";
 import {DashboardComponent} from "./dashboard/dashboard.component";
+import {authGuard} from "./guards/auth.guard";
 
 export const routes: Routes = [
   {path: '', pathMatch: 'full', redirectTo: 'dashboard'},
   // { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.routes').then(m => m.WELCOME_ROUTES) }
-  {path: 'dashboard', loadComponent: () => DashboardComponent},
+  {path: 'dashboard', loadComponent: () => DashboardComponent, canActivate: [authGuard]},
   {path: 'auth', loadComponent: () => AuthComponent}
 ];
