c76b9bd3edaf6bb388298cdd49ebbc7a0e53371d
Tanbin Himel <tanbin.hossain@trucklagbe.com>
2024-05-29 13:31:27 +0600

    #master route added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, WritableSignal} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {RouterOutlet} from '@angular/router';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {NzIconModule} from 'ng-zorro-antd/icon';
 import {NzLayoutModule} from 'ng-zorro-antd/layout';
 import {NzMenuModule} from 'ng-zorro-antd/menu';
@@ -12,7 +12,7 @@ import {NzAvatarComponent} from "ng-zorro-antd/avatar";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, HeaderComponent, NzAvatarComponent],
+  imports: [CommonModule, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule, HeaderComponent, NzAvatarComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
