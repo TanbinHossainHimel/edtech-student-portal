9fe1b78b4495c8d12bf6ce636d17531706858059
Tanbin Himel <tanbin.hossain@trucklagbe.com>
2024-09-02 17:31:22 +0600

    #master auth implemented

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import {Injectable, signal, WritableSignal} from '@angular/core';
 import {LocalStorageService} from "./local-storage.service";
 import {Router} from "@angular/router";
 import {SocialAuthService, SocialUser} from "@abacritt/angularx-social-login";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
@@ -9,14 +10,16 @@ import {SocialAuthService, SocialUser} from "@abacritt/angularx-social-login";
 export class AuthService {
   isUserAuthorized: WritableSignal<boolean> = signal(this.isAccessTokenAvailable());
 
-  constructor(private localStorageService: LocalStorageService, private router: Router, private socialAuthService: SocialAuthService) {
+  constructor(private localStorageService: LocalStorageService, private router: Router, private socialAuthService: SocialAuthService, private http: HttpClient) {
   }
 
-  signInUserWithGoogle(socialUser: SocialUser) {
-    console.log(socialUser);
-    this.localStorageService.setData('access-token', 'himel');
-    this.isUserAuthorized.update(() => this.isAccessTokenAvailable());
-    this.router.navigate(['dashboard']).then();
+  signInUserWithGoogle(socialUser: any) {
+    // const headers = this.httpHeaders.append('Accept', 'application/json');
+    return this.http.post('http://localhost:3000/auth/sign-in', socialUser);
+    // console.log(socialUser);
+    // this.localStorageService.setData('access-token', 'himel');
+    // this.isUserAuthorized.update(() => this.isAccessTokenAvailable());
+    // this.router.navigate(['dashboard']).then();
   }
 
 
