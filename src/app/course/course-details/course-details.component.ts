019910d63ddf443074c9156b7d7bf4191c1b122b
TanbinHossainHimel <156938064+TanbinHossainHimel@users.noreply.github.com>
2024-10-09 17:40:27 +0600

    #master course details updated

--- src/app/course/course-details/course-details.component.ts
+++ src/app/course/course-details/course-details.component.ts
@@ -5,6 +5,7 @@ import {AsyncPipe, JsonPipe} from "@angular/common";
 import {NzCollapseComponent, NzCollapsePanelComponent} from "ng-zorro-antd/collapse";
 import {VideoPlayerComponent} from "../../shared/video-player/video-player.component";
 import {NzButtonComponent} from "ng-zorro-antd/button";
+import {NzModalModule, NzModalService} from "ng-zorro-antd/modal";
 
 @Component({
   selector: 'app-course-details',
@@ -15,7 +16,8 @@ import {NzButtonComponent} from "ng-zorro-antd/button";
     NzCollapseComponent,
     VideoPlayerComponent,
     JsonPipe,
-    NzButtonComponent
+    NzButtonComponent,
+    NzModalModule
   ],
   templateUrl: './course-details.component.html',
   styleUrl: './course-details.component.scss'
@@ -23,10 +25,17 @@ import {NzButtonComponent} from "ng-zorro-antd/button";
 export class CourseDetailsComponent implements OnInit {
   private activatedRouter: ActivatedRoute = inject(ActivatedRoute);
   private courseService: CourseService = inject(CourseService);
+  private modal: NzModalService = inject(NzModalService);
 
   course$ = this.courseService.findOne(parseInt(this.activatedRouter.snapshot.paramMap.get('id')!));
 
   ngOnInit() {
   }
 
+  onClickBuyNow() {
+    this.modal.create({
+      nzTitle: 'Title',
+      nzCentered: true,
+    })
+  }
 }
